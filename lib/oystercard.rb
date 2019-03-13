aad14171da41e5eb52fd5c90dd180cc0217b3d63
Arthur Ashman <arthurashman@gmail.com>
2019-03-13 15:42:23 +0000

    Finished step 8 all tests passing

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,11 +5,28 @@ class Oystercard
 
   def initialize(balance = START_BALANCE)
     @balance = balance
+    @journey = nil
   end
 
   def top_up(amount)
     fail 'Max balance reached' if (amount + balance) > MAX_BALANCE
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @journey = true
+  end
+
+  def touch_out
+    @journey = false
+  end
+
+  def in_journey?
+    @journey
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,25 +3,54 @@ require 'oystercard'
 describe Oystercard do
 
   subject(:card) {Oystercard.new}
-  describe '#Balance' do
 
-    it 'default balance is zero' do
-      expect(card.balance).to eq Oystercard::START_BALANCE
-    end
+  it 'allows the customer to touch in and pass through the barriers' do
+    expect(card).to respond_to(:touch_in)
   end
 
-  describe '#Top_up' do
-    it 'Responds to top_up method' do
-      expect(card).to respond_to(:top_up)
-    end
+  describe '#balance' do
 
-    it 'Balance should change when topped up' do
-      expect(card.top_up(5)).to eq(5)
-    end
+      it 'default balance is zero' do
+          expect(card.balance).to eq Oystercard::START_BALANCE
+      end
   end
 
-  it 'Error msg raised when max balance reached' do
-    expect{ card.top_up(100) }.to raise_error('Max balance reached')
+  describe '#top_up' do
+
+      it 'Responds to top_up method' do
+          expect(card).to respond_to(:top_up)
+      end
+
+      it 'Balance should change when topped up' do
+          expect(card.top_up(5)).to eq(5)
+      end
+
+      it 'Error msg raised when max balance reached' do
+          expect{ card.top_up(100) }.to raise_error('Max balance reached')
+      end
   end
 
+
+
+  describe '#deduct' do
+      it 'responds to deduct method' do
+          expect(card).to respond_to(:deduct)
+      end
+
+      it 'balance should change when deducted' do
+          expect(card.deduct(2)).to eq(-2)
+      end
+  end
+
+  describe '#in_journey?' do
+    it 'returns true if oystercard is in journey' do
+      card.touch_in
+      expect(card).to be_in_journey
+    end
+    it 'returns false if oystercard is not in journey' do
+      card.touch_in
+      card.touch_out
+      expect(card).to_not be_in_journey
+    end
+  end
 end
